56025742de2cbba2c55cf59ccedf098b6899c8a2
Armaan Gogoi <agogoi_be23@thapar.edu>
2025-04-09 07:01:51 +0530

    added required validation in student register

--- backend/routes/student.routes.js
+++ backend/routes/student.routes.js
@@ -9,11 +9,13 @@ const {authStudent} = require('../middlewares/auth.middleware');
 router.post("/register", [
     body('email').isEmail().withMessage('Please enter a valid email'),
     body('phone').optional().isLength({ min: 10, max: 10 }).withMessage('Please enter a valid phone number'),
-    body('rollNumber').isLength({ min: 8, max: 15 }).withMessage('Please enter a valid roll number'),
+    body('roll_number').isLength({ min: 8, max: 15 }).withMessage('Please enter a valid roll number'),
     body('semester').notEmpty().withMessage('Please enter a valid semester type'),
     body('branch').notEmpty().withMessage('Please enter a valid branch'),
     body('name').isLength({ min: 3 }).withMessage('Name must be at least 3 characters long'),
-    body('password').isLength({ min: 3 }).withMessage('Password must be at least 3 characters long')
+    body('password').isLength({ min: 3 }).withMessage('Password must be at least 3 characters long'),
+    body('year_of_passing').notEmpty().withMessage('Please enter a valid year of passing'),
+    body('current_cgpa').notEmpty().withMessage('Please enter a valid cgpa')
 ], studentController.Register);
 
 router.post("/login",[
