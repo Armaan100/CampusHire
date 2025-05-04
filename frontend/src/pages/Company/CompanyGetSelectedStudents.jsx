07ac7e1eb1be8f73de94435185b442cc5daf1e90
Armaan Gogoi <agogoi_be23@thapar.edu>
2025-05-04 11:34:18 +0530

    added company get selected students

--- frontend/src/pages/Company/CompanyGetSelectedStudents.jsx
+++ frontend/src/pages/Company/CompanyGetSelectedStudents.jsx
@@ -1,10 +1,59 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { useParams, Link } from "react-router-dom";
+import StudentCard from "./components/StudentCard";
 
-const GetSelectedStudents = () => {
-    return (
+const CompanyGetSelectedStudents = () => {
+  const [students, setStudents] = useState([]);
+  const {jobId} = useParams();
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    const fetchStudents = async () => {
+      try {
+        const token = localStorage.getItem("token");
+        const response = await axios.get(
+          `http://localhost:5000/company/get-selected-students/${jobId}`,
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+        console.log(response.data);
+        setStudents(response.data.students || []);
+      } catch (err) {
+        console.error("Error fetching jobs:", err);
+        setError("Failed to load jobs. Please try again.");
+        setStudents([]);
+      }
+    };
+
+    fetchStudents();
+  }, []);
+
+  return (
+    <div className="max-w-3xl mx-auto mt-8">
+      <h1 className="text-2xl font-bold mb-6 text-center text-purple-800">
+        Selected Students
+      </h1>
+
+      {error && <p className="text-red-600 text-center">{error}</p>}
+
+      {students.length === 0 && !error ? (
         <div>
-        
+            <p className="text-gray-500 text-center">No students were selected.</p>
+            <p className="text-gray-500 text-center">Your Evaluation is completed Successfully</p>
+            <p className="text-gray-500 text-center">Thank You</p>
         </div>
-    );
-}
+      ) : (
+        students.map((student) => <StudentCard key={student.roll_number} student={student} />)
+      )}
+      <p className="text-gray-500 text-center">Your evaluation process is completed successfully</p>
+      <p className="text-gray-500 text-center">Thank you for using our platform! We hope you had a great experience.</p>
+      <p className="text-gray-500 text-center">If you had any difficulty than please do contact us from the <Link to="/about"><span className="font-bold">about us</span></Link> page.</p>
+    </div>
+  );
+};
 
-export default GetSelectedStudents;
+export default CompanyGetSelectedStudents;
--- frontend/src/pages/Company/components/StudentCard.jsx
+++ frontend/src/pages/Company/components/StudentCard.jsx
@@ -5,25 +5,25 @@ const StudentCard = ({ student }) => {
 
   return (
     <div className="bg-white shadow-md rounded-2xl p-6 mb-4 border border-gray-200">
-      <div className="text-sm text-gray-600 mb-1">
+      <div className="text-sm text-gray-600 mb-2">
         <span className="font-medium">Roll Number:</span> {student.roll_number}
       </div>
-      <div className="text-sm text-gray-600 mb-1">
+      <div className="text-sm text-gray-600 mb-2">
         <span className="font-medium">Name:</span> {student.name}
       </div>
-      <div className="text-sm text-gray-600 mb-4">
+      <div className="text-sm text-gray-600 mb-2">
         <span className="font-medium">Email:</span> {student.email}
       </div>
-      <div className="text-sm text-gray-600 mb-4">
+      <div className="text-sm text-gray-600 mb-2">
         <span className="font-medium">Phone Number:</span> {student.phone}
       </div>
-      <div className="text-sm text-gray-600 mb-4">
+      <div className="text-sm text-gray-600 mb-2">
         <span className="font-medium">Branch:</span> {student.branch}
       </div>
-      <div className="text-sm text-gray-600 mb-4">
+      <div className="text-sm text-gray-600 mb-2">
         <span className="font-medium">Semester:</span> {student.semester}
       </div>
-      <div className="text-sm text-gray-600 mb-4">
+      <div className="text-sm text-gray-600 mb-2">
         <span className="font-medium">Location:</span> {student.city}, {student.state}
       </div>
     </div>
