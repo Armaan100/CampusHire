83442ed293bd6ff8a4ab9c1c9e3c35c4569833da
Armaan Gogoi <agogoi_be23@thapar.edu>
2025-05-04 11:24:20 +0530

    added student card frontend

---
+++ frontend/src/pages/Company/components/StudentCard.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+const StudentCard = ({ student }) => {
+
+  return (
+    <div className="bg-white shadow-md rounded-2xl p-6 mb-4 border border-gray-200">
+      <div className="text-sm text-gray-600 mb-1">
+        <span className="font-medium">Roll Number:</span> {student.roll_number}
+      </div>
+      <div className="text-sm text-gray-600 mb-1">
+        <span className="font-medium">Name:</span> {student.name}
+      </div>
+      <div className="text-sm text-gray-600 mb-4">
+        <span className="font-medium">Email:</span> {student.email}
+      </div>
+      <div className="text-sm text-gray-600 mb-4">
+        <span className="font-medium">Phone Number:</span> {student.phone}
+      </div>
+      <div className="text-sm text-gray-600 mb-4">
+        <span className="font-medium">Branch:</span> {student.branch}
+      </div>
+      <div className="text-sm text-gray-600 mb-4">
+        <span className="font-medium">Semester:</span> {student.semester}
+      </div>
+      <div className="text-sm text-gray-600 mb-4">
+        <span className="font-medium">Location:</span> {student.city}, {student.state}
+      </div>
+    </div>
+  );
+};
+
+export default StudentCard;
